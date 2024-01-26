65bd680a414c78d8db7d219fd3ddebb8e362c588
Ricky Tang <yizhe.tang@progressfactors.com>
2024-01-26 22:13:05 +0000

    Initial commit
    MVP implemented

---
+++ main.py
@@ -0,0 +1,32 @@
+from DirectoryWatcher import Watcher
+import os
+
+
+def main():
+    path_to_watch = "_target_"
+    # os.chmod(path_to_watch, 0o777)  # set the dir to readable, writable and executable
+    w = Watcher(path_to_watch)
+    w.run()
+
+
+if __name__ == '__main__':
+    main()
+
+'''
+Steps to take (MVP):
+1. Put an .rar into a folder 
+2. The script will unzip the .rar file (done)
+3. Add a translucent png watermark to all the .png files in that folder
+4. Compress all watermarked images into .jpg and make sure they are 1200 pixels height with the same aspect ratio as the original
+5. Pack the processed images in to a folder and compress it into a zip file
+
+TODO:
+1. More configurations:
+    - refactor: break up the file and move configs to main or a single file
+    - support .jpg
+    - multithreading for adding watermark and compressing image
+
+2. Add a GUI
+
+3. Batch process (multiple rar files?)
+'''
